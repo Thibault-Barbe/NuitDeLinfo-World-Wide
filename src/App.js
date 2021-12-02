21ec3883e2a9ac6e376a9d12847d7899b66a1cc3
TIBO SINJE <thibault.barbe@epitech.eu>
2021-12-02 22:33:15 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 
 
 function App() {
   const { t, i18n } = useTranslation();
+  const [culture, setCulture] = useState("uk");
   return (
     <div className="App">
       <header className="App-header">
-      <h1>{t("title")}</h1>
-        <button onClick={() => {i18n.changeLanguage("fr");}}>fr</button>
-        <button onClick={() => {i18n.changeLanguage("en");}}>en</button>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>{t("title-"+culture)}</h1>
+      <button onClick={() => {i18n.changeLanguage("fr");setCulture("fr")}}>fr</button>
+      <button onClick={() => {i18n.changeLanguage("fr");setCulture("ca")}}>fr-ca</button>
+      <button onClick={() => {i18n.changeLanguage("en");setCulture("uk")}}>en-uk</button>
+      <button onClick={() => {i18n.changeLanguage("en");setCulture("usa")}}>en-usa</button>
+      <button onClick={() => {i18n.changeLanguage("en");setCulture("aus")}}>en-aus</button>
         <a
           className="App-link"
           href="https://reactjs.org"
